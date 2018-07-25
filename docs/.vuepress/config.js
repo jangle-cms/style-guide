739976871b9794c9944cfbe5bf5752bfaee2a4e9
Rene Hernandez <rhernandez@onenorth.com>
2018-07-24 21:07:50 -0500

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: "Jangle",
+  description: "a cms for humans."
+}
---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <div v-if="$page.frontmatter.home">Home</div>
+    <ul>
+      <li v-for="item in $page.frontmatter.items">{{item}}</li>
+    </ul>
+    <Content/>
+  </div>
+</template>
+
+<style lang="scss">
+  .h1 {
+    font-family: sans-serif;
+  }
+</style>
